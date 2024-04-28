682585c93ee2f92d0d65122b14ff07a5a7318e98
xxLovy <1xxlovyxx1@gmail.com>
2024-04-28 15:48:44 +0800

    clerk webhook

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,17 @@
-// import { authMiddleware } from "@clerk/nextjs/server";
+import { authMiddleware } from "@clerk/nextjs/server";
 
-// export default authMiddleware({});
-
-// export const config = {
-//     matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
-// };
-
-import { clerkMiddleware } from "@clerk/nextjs/server";
-
-export default clerkMiddleware();
+export default authMiddleware({
+    publicRoutes: ['/api/webhooks/clerk']
+});
 
 export const config = {
     matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
 };
+
+// import { clerkMiddleware } from "@clerk/nextjs/server";
+
+// export default clerkMiddleware();
+
+// export const config = {
+//     matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+// };
