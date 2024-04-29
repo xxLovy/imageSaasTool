497c24f605843a4e3925197c7b3840ae87bc26c4
xxLovy <1xxlovyxx1@gmail.com>
2024-04-29 09:08:52 +0800

    debugging webhook

--- src/lib/database/mongoose.ts
+++ src/lib/database/mongoose.ts
@@ -2,30 +2,45 @@ import mongoose, { Mongoose } from 'mongoose'
 
 const MONGODB_URL = process.env.MONGODB_URL
 
-interface MongooseConnection {
-    conn: Mongoose | null;
-    promise: Promise<Mongoose> | null;
-}
-
-// caching connection?? because the serverless Next.js
-let cached: MongooseConnection = (global as any).mongoose
-
-if (!cached) {
-    cached = (global as any).mongoose = {
-        conn: null, promise: null
-    }
-}
 
 export const connectToDatabase = async () => {
-    if (cached.conn) return cached.conn;
+    if (!MONGODB_URL) {
+        throw new Error('Missing MongoDB URL');
+    }
 
-    if (!MONGODB_URL) throw new Error('Missing MongoDB URL')
-
-    cached.promise = cached.promise || mongoose.connect(MONGODB_URL, {
-        dbName: "IMAGINARY-XX",
+    // 每次调用都尝试创建新的连接
+    const conn = await mongoose.connect(MONGODB_URL, {
+        dbName: "IMAGINARY-XX", // 确保数据库名称与实际的数据库匹配
         bufferCommands: false,
-    })
+    });
 
-    cached.conn = await cached.promise
-    return cached.conn
-}
+    return conn;
+};
+
+// interface MongooseConnection {
+//     conn: Mongoose | null;
+//     promise: Promise<Mongoose> | null;
+// }
+
+// // caching connection?? because the serverless Next.js
+// let cached: MongooseConnection = (global as any).mongoose
+
+// if (!cached) {
+//     cached = (global as any).mongoose = {
+//         conn: null, promise: null
+//     }
+// }
+
+// export const connectToDatabase = async () => {
+//     if (cached.conn) return cached.conn;
+
+//     if (!MONGODB_URL) throw new Error('Missing MongoDB URL')
+
+//     cached.promise = cached.promise || mongoose.connect(MONGODB_URL, {
+//         dbName: "IMAGINARY-XX",
+//         bufferCommands: false,
+//     })
+
+//     cached.conn = await cached.promise
+//     return cached.conn
+// }
